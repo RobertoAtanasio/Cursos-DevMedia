324eee3cf1ac288298f4b5e16fbe914a7ec6856e
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-09-30 18:00:35 -0300

    Componentes JSF2

--- jsfappcdi/src/main/java/br/com/rapl/jsfappcdi/controle/ControleComponentes.java
+++ jsfappcdi/src/main/java/br/com/rapl/jsfappcdi/controle/ControleComponentes.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean(name="controleComponentes")
 @SessionScoped
@@ -12,14 +14,24 @@ public class ControleComponentes implements Serializable {
 	private static final long serialVersionUID = -2411877451092479285L;
 	
 	private String texto;
+	private String idComponente;
 	
-	public ControleComponentes(){
-		
-	}
+	public ControleComponentes(){}
 	
 	public String geraTexto(){
 		texto = "Texto gerado: < > & <b>Texto em negrito</b>";
 		return "componentesParte2";
+	}
+	
+	public String executar() {
+		return "componentesParte3";
+	}
+	// Criar um evento listener para capturar o componente onde houve a ação
+	
+	public void listener (ActionEvent event) {
+		UIComponent source = event.getComponent();
+		idComponente = "Componenet que executar a ação: " + source.getId();
+		System.out.println(">>> Componente: " + idComponente);
 	}
 
 	public String getTexto() {
@@ -29,4 +41,13 @@ public class ControleComponentes implements Serializable {
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
+	public String getIdComponente() {
+		return idComponente;
+	}
+
+	public void setIdComponente(String idComponente) {
+		this.idComponente = idComponente;
+	}
+
 }
